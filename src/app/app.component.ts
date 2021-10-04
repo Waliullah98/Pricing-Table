fda9b1047416e1200356c61f53712bc0fb28bd40
Sayyed Waliullah <jiems2020@gmail.com>
2021-10-04 16:05:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,156 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'price-Table';
+  img1='./assets/about.jpg';
+  monk='Monkfox Study';
+
+
+pricingData = [
+  {
+    type : "FREE",
+    price : 0,
+    duration: "month",
+    details:[
+      {
+        title : "Single User",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "5GB Storage",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Unlimitted Public Projects",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Community Access",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Unlimitted Private Projects",
+        isAvailable : false,
+        boldLetter : false
+      },
+      {
+        title : "Dedicated Phone Support",
+        isAvailable : false,
+        boldLetter : false
+      },
+      {
+        title : "Free SubDomain",
+        isAvailable : false,
+        boldLetter : false
+      },
+      {
+        title : "Monthly Status Reports",
+        isAvailable : false,
+        boldLetter : false
+      }
+    ]
+  },
+  {
+    type : "PLUS",
+    price : 9,
+    duration: "month",
+    details:[
+      {
+        title : "5 User",
+        isAvailable : true,
+        boldLetter : true
+      },
+      {
+        title : "50GB Storage",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Unlimitted Public Projects",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Community Access",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Unlimitted Private Projects",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Dedicated Phone Support",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Free SubDomain",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Monthly Status Reports",
+        isAvailable : false,
+        boldLetter : false
+      }
+    ]
+  },
+  {
+    type : "PRO",
+    price : 49,
+    duration: "month",
+    details:[
+      {
+        title : "Unlimitted Users",
+        isAvailable : true,
+        boldLetter : true
+      },
+      {
+        title : "150GB Storage",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Unlimitted Public Projects",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Community Access",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Unlimitted Private Projects",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Dedicated Phone Support",
+        isAvailable : true,
+        boldLetter : false
+      },
+      {
+        title : "Unlimitted Free SubDomain",
+        isAvailable : true,
+        boldLetter : true
+      },
+      {
+        title : "Monthly Status Reports",
+        isAvailable : true,
+        boldLetter : false
+      }
+    ]
+  }
+];
+  
 }
+
+
+
+
